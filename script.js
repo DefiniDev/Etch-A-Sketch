42acdd9aba3b206440e066c207e0defd5ce02c65
DefiniDev <definidev@gmail.com>
2021-09-07 11:48:06 +0100

    Complete UI / top button row functionality
    - Finish and test logic for all top row buttons
    - Initial code for bottom row buttons
    - Complete footer with personal Github link

--- script.js
+++ script.js
@@ -1,47 +1,158 @@
 "use strict";
-// selectors
-const btnNewGrid = document.querySelector("button");
-const gridSizeInput = document.querySelector("input");
-const gridContainer = document.getElementById("grid-container");
+window.onload = () => {
+  // disable drag behaviour
+  window.ondragstart = function () {
+    return false;
+  };
 
-// div-creation function
-const divCreate = sideLength => {
-  for (let i = 1; i <= sideLength; i++) {
-    window["cell" + i] = document.createElement("div");
-    window["cell" + i].classList.add("cell");
-    window["cell" + i].classList.add("cell" + i);
-    gridContainer.appendChild(window["cell" + i]);
-    // add mouseover listener for div color change
-    window["cell" + i].addEventListener("mouseover", () => {
-      window["cell" + i].style.backgroundColor = "#000";
+  // selectors & variables
+  let currentGridSize = 100;
+  let divChangeEffect = "#000";
+  const btnGrid = document.getElementById("btn-grid");
+  const btn10x = document.getElementById("btn-10x");
+  const btn50x = document.getElementById("btn-50x");
+  const btn100x = document.getElementById("btn-100x");
+  const btnPen = document.getElementById("btn-pen");
+  const pen = document.getElementById("pen");
+  const btnLighten = document.getElementById("btn-lighten");
+  const btnDarken = document.getElementById("btn-darken");
+  const btnRainbow = document.getElementById("btn-rainbow");
+  const gridContainer = document.getElementById("grid-container");
+
+  // addEventListeners function
+  const eventListeners = (divAmnt, divChange) => {
+    window["cell" + divAmnt].addEventListener("mouseover", e => {
+      if (e.buttons === 1)
+        window["cell" + divAmnt].style.backgroundColor = divChange;
     });
-  }
-};
-// create first grid & manual layout
-divCreate(256);
-gridContainer.style.grid = "repeat(16, 1fr) / repeat(16,1fr)";
+    window["cell" + divAmnt].addEventListener("mousedown", e => {
+      window["cell" + divAmnt].style.backgroundColor = divChange;
+    });
+  };
 
-btnNewGrid.addEventListener("click", () => {
-  // ask for new grid length to create row/column value; sqr result = total divs to pass grid create function
+  // div-creation function
+  const divCreate = sideLength => {
+    for (let i = 1; i <= sideLength; i++) {
+      window["cell" + i] = document.createElement("div");
+      // window["cell" + i].classList.add("cell");
+      // window["cell" + i].classList.add("cell" + i);
+      window["cell" + i].classList.add("grid-visible");
+      gridContainer.appendChild(window["cell" + i]);
+      // add event-listeners for mouse-over (requires mouse1 down = true) and mouse-down
+      eventListeners(i, divChangeEffect);
+    }
+  };
 
-  const gridLength = Number(gridSizeInput.value);
-
-  if (gridLength > 100) {
-    gridSizeInput.value = "";
-    gridSizeInput.placeholder = "Size too large.";
-  } else if (gridLength === 0) {
-    gridSizeInput.placeholder = "Please enter size...";
-  } else {
-    // remove previous grid divs
+  // div-removal function
+  const divRemove = () => {
     while (gridContainer.firstChild) {
       gridContainer.removeChild(gridContainer.firstChild);
     }
-    // create new user-specific grid
-    const gridNewSize = Math.pow(gridLength, 2);
-    divCreate(gridNewSize);
-    // use initial user specified grid-length for row/column value
-    gridContainer.style.grid = `${"repeat(" + gridLength + ",1fr)"} / ${
-      "repeat(" + gridLength + ",1fr)"
-    }`;
-  }
-});
+  };
+
+  // Change Eventlisteners function
+  // ------------------------------------------------------------------------
+  const changeEventListeners = (divAmnt, divChange) => {
+    console.log(divAmnt);
+    console.log(divChange);
+    for (let i = 1; i <= divAmnt; i++) {
+      window["cell" + i].addEventListener("mouseover", e => {
+        if (e.buttons === 1)
+          window["cell" + i].style.backgroundColor = divChange;
+      });
+      window["cell" + i].addEventListener("mousedown", e => {
+        window["cell" + i].style.backgroundColor = divChange;
+      });
+    }
+  };
+  // ------------------------------------------------------------------------
+
+  // create first grid & manual layout
+  divCreate(currentGridSize);
+  gridContainer.style.grid = "repeat(10, 1fr) / repeat(10,1fr)";
+
+  // Grid btns
+  btnGrid.addEventListener("click", () => {
+    btnGrid.classList.toggle("btn-active");
+    for (let i = 1; i <= currentGridSize; i++) {
+      window["cell" + i].classList.toggle("grid-invisible");
+    }
+  });
+
+  btn10x.addEventListener("click", () => {
+    currentGridSize = 100;
+    btnGrid.classList.add("btn-active");
+    btn10x.classList.add("btn-active");
+    btn50x.classList.remove("btn-active");
+    btn100x.classList.remove("btn-active");
+    divRemove();
+    divCreate(currentGridSize);
+    gridContainer.style.grid = "repeat(10, 1fr) / repeat(10,1fr)";
+  });
+
+  btn50x.addEventListener("click", () => {
+    currentGridSize = 2500;
+    btnGrid.classList.add("btn-active");
+    btn10x.classList.remove("btn-active");
+    btn50x.classList.add("btn-active");
+    btn100x.classList.remove("btn-active");
+    divRemove();
+    divCreate(currentGridSize);
+    gridContainer.style.grid = "repeat(50, 1fr) / repeat(50,1fr)";
+  });
+
+  btn100x.addEventListener("click", () => {
+    currentGridSize = 10000;
+    btnGrid.classList.add("btn-active");
+    btn10x.classList.remove("btn-active");
+    btn50x.classList.remove("btn-active");
+    btn100x.classList.add("btn-active");
+    divRemove();
+    divCreate(currentGridSize);
+    gridContainer.style.grid = "repeat(100, 1fr) / repeat(100,1fr)";
+  });
+
+  btnPen.addEventListener("click", () => {
+    btnPen.classList.add("btn-active");
+    btnLighten.classList.remove("btn-active");
+    btnDarken.classList.remove("btn-active");
+    btnRainbow.classList.remove("btn-active");
+    console.log(pen.value);
+    changeEventListeners(currentGridSize, pen.value);
+  });
+
+  // --- Future reference for Darken / Light solution ---
+  //   a {
+  //     /* a nice, modern blue for links */
+  //     color: #118bee;
+  // }
+  // a:active {
+  //     /* Darken on click by 15% (down to 85%) */
+  //     filter: brightness(0.85);
+  // }
+
+  btnLighten.addEventListener("click", () => {
+    btnPen.classList.remove("btn-active");
+    btnLighten.classList.add("btn-active");
+    btnDarken.classList.remove("btn-active");
+    btnRainbow.classList.remove("btn-active");
+  });
+
+  btnDarken.addEventListener("click", () => {
+    btnPen.classList.remove("btn-active");
+    btnLighten.classList.remove("btn-active");
+    btnDarken.classList.add("btn-active");
+    btnRainbow.classList.remove("btn-active");
+    changeEventListeners();
+  });
+
+  // Rainbow btn
+
+  btnRainbow.addEventListener("click", () => {
+    btnPen.classList.remove("btn-active");
+    btnLighten.classList.remove("btn-active");
+    btnDarken.classList.remove("btn-active");
+    btnRainbow.classList.add("btn-active");
+    changeEventListeners(currentGridSize, test());
+  });
+};
